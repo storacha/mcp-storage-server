e93e3884797919249dfd19d0e8a9e9469964fcc0
Felipe Forbeck <felipe.forbeck@gmail.com>
2025-03-26 14:36:18 -0300

    removed sample tool

--- src/core/server/tools/index.ts
+++ src/core/server/tools/index.ts
@@ -1,15 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { helloTool } from "./hello.js";
 import { uploadTool } from "./upload.js";
 
 export const registerTools = (server: McpServer) => {
-  server.tool(
-    helloTool.name,
-    helloTool.description,
-    helloTool.inputSchema.shape,
-    helloTool.handler
-  );
-
   server.tool(
     uploadTool.name,
     uploadTool.description,
